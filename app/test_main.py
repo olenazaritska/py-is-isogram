30ca8fff7ed9f6380e860bfddbcd088c4b12f1a8
Olena Zaritska <zaritskaos@gmail.com>
2024-05-21 10:48:40 +0300

    implement py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,52 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "entry_string, expected_output",
+        [
+            pytest.param(
+                "playgrounds",
+                True,
+                id="should check all lowercase and return True"
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="should check all lowercase and return False"
+            ),
+            pytest.param(
+                "Flower",
+                True,
+                id="should check capitalized and return True"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="should check capitalized and return False"
+            ),
+            pytest.param(
+                "fLoWer",
+                True,
+                id="should check mixed casing and return True"
+            ),
+            pytest.param(
+                "MiSsinG",
+                False,
+                id="should check mixed casing and return False"
+            ),
+            pytest.param(
+                "",
+                True,
+                id="should check empty string and return True"
+            ),
+        ]
+    )
+    def test_correct_assessment_if_isogram(
+            self,
+            entry_string: str,
+            expected_output: bool
+    ) -> None:
+        assert is_isogram(entry_string) == expected_output
